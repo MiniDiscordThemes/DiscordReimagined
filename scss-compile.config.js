51ed1dc4cb6b0f9e0ffc2a8936c57b4583cf3f39
Saltssaumure <themes@saltssaumure.net>
2024-06-20 03:10:36 +0100

    Release v1.1.1

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -2,7 +2,7 @@
 export default {
 	meta: {
 		name: "Discord Reimagined" /* The official name of your theme. */,
-		scss: "reimagine-replugged" /* The name of your base scss file. */,
+		scss: "reimagine" /* The name of your base scss file. */,
 		repo: "DiscordReimagined" /* The name of your theme repository. */,
 		version: "dev" /* The version of your theme. */
 	}
